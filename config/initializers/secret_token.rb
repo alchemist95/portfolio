7b52916b3075d8798379a088544ba688e219625f
alchemist95 <shashank.9d@gmail.com>
2015-08-05 22:47:52 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threadly::Application.config.secret_key_base = '32efc35cecd379b9d98bcaa7bedb24c7f8729092ed635e08cbf5db255a6569dcb1847aaac6ff51d5e0e3fa795ba46e68865e7214dc7769c57b916b4987dc28c3'
